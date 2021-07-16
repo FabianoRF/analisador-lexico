c1a45ff01a9ca732bb4832deb40882f3fa47fa5f
fabianorf <freis884@gmail.com>
2021-07-15 21:39:22 -0300

    afd e filtro funcional

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,38 +1,64 @@
 package com.main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+// TODO:completar simbolos e colocar liral no processa palavras
+//
 
+
+public class Main {
     public static List<String> palavrasReservadas;
-    public static int contLexema = 0;
-    public static List<LinhaTabela> tabela = new ArrayList<>();
+    public static List<String> linhas = new ArrayList<>();
+    public static List<LinhaTabela> listaLinhaTabela = new ArrayList<>();
+    public static String simbolos = "()=,+-"; //completar
+    public static int contId = 0;
+
+    public static void leitor(){
+        String nomeArq = "entrada.txt";
+        String texto = "";
+        try {
+            FileReader arq = new FileReader(nomeArq);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = "";
+
+            while (true) {
+                if (linha != null) {
+                    linhas.add(linha);
+                } else
+                    break;
+                linha = lerArq.readLine();
+            }
+            lerArq.close();
+            System.out.println("\nLeitura concluída!\n");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Erro na leitura do arquivo!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro na leitura do arquivo!");
+        }
+    }
 
     public static void main(String[] args) {
-
         populaPalavrasReservadas();
+        leitor();
 
-        // passo 2: criar leitura
-        String ex = "program circle\n   implicit none\n   real r=1.0, pi=0\n   parameter(pi = 3.14159)\n   write(\"Raio:\")\n   write(\"Raio:\")\nsstop";
-//        System.out.println(ex);
+        processaTexto();
 
-
-        processaTexto(ex);
-
-        LinhaTabela linhaTabela = new LinhaTabela(0 , "a", "a", "a", "a", "a", "a",0 );
-
-        insereTabela(linhaTabela, 0);
-
-        Tabela exibeTabela = new Tabela();
-        exibeTabela.exibeTabela((ArrayList) tabela);
-
+        Tabela tabela = new Tabela();
+        tabela.exibeTabela((ArrayList) listaLinhaTabela);
     }
 
 
-    public static void processaTexto(String texto) {
-        // 1 separa por linhas, armazena
+    public static void processaTexto() {
+        // separa por linhas, armazena
+        // passa um filtro retirando comentários
         // passa um filtro retirando o que esta sob parenteses
         // passa um filtro em todos simbolos () e afins, colocando um espaço no lugar
         // add os simbolos por meio do insereTabela
@@ -41,105 +67,103 @@ public class Main {
         // aqui lidar antes de passar pro AFD tratar exemplos como "parameter(pi = 3.14159)" que não serão separados por espaço nas etapas anteriores
         // o que sobrar vai para o AFD cuidar
 
-        List<String> linhas = Arrays.asList(texto.split("\n"));
-
         // inicio do filtro da linha
-
-        String simbolos = "()=,"; //completar
-
-
         for(int l = 0; l< linhas.size(); l++){
             String linha = linhas.get(l);
 
-
-
-            // filtro de conteudo entre parenteses aqui so esta removendo falta adicionar como literal
-            String regex = "\"([^\"]*)\"";
+            // filtro remove comentário
+            String regex = "!.*";
             linha = linha.replaceAll(regex, " ");
-//            insereTabela(String.valueOf(letra), l);
+
+            // TODO: adicionar na tabela quando identificado um literal
+            // filtro de conteudo entre parenteses aqui so esta removendo falta adicionar como literal
+            regex = "\"([^\"]*)\"";
+            linha = linha.replaceAll(regex, " ");
+            LinhaTabela linhaTabela = new LinhaTabela(contId, String.valueOf(linha), "SIMBOLO", "-", "-", "-", "-", l);
+//            insereTabela(linhaTabela, l);
 
 
-            // filtro de simbolos funcional, falta mapear todos simbolos
+            // filtro de simbolos
+            // Aqui é verificado se os simbolos estão contidos na constante simbolo, se sim são adicionados na tabela
             for(int i = 0; i < linha.length(); i++){
                 char letra = linha.charAt(i);
                 if(simbolos.contains(String.valueOf(letra))){
-//                    insereTabela(String.valueOf(letra), l);
+                    linhaTabela = new LinhaTabela(contId, String.valueOf(letra), "SIMBOLO", "-", "-", "-", "-", i);
+                    insereTabela(linhaTabela, i);
                     linha = linha.replaceFirst("\\" + letra, " ");
                 }
             }
+            // adiciona na lista de linhas
             linhas.set(l, linha);
 
-            // ao final dos filtros fazer um split por espaço e encaminhar cada palavra para o AFD
+            // ao final dos filtros é feito um split por espaço e as palavras são encaminhadas para o AFD
             List<String> palavras = Arrays.asList(linha.split(" ")); // aqui há um problema com separação por espaços não resolvido
-
             for(String palavraAtual: palavras) {
-                executaAFD(palavraAtual); // aqui nosso amigo AFD entra em ação
+                executaAFD(palavraAtual, l); // chama o AFD
             }
         }
-
-
-
-        for (String linha: linhas) {
-            System.out.println("\n"+linha);
-        }
-
     }
 
-    public static boolean executaAFD(String palavra) {
+    public static void executaAFD(String palavra, int nLinha) {
+        // verifica se não foi mandado um espaço vazio
+        if (!palavra.contains(" ") && !palavra.isEmpty()) {
+            int  i=0;
+            boolean flag = true, status = true;
+            char primeiroCaracterAlpha = palavra.charAt(0);//pega o primeiro caracter da string
+            int tamPalavra = palavra.length();//tamanho da string
+            LinhaTabela linhaTabela;
 
-        int  i=0;
+            if (Character.isAlphabetic(primeiroCaracterAlpha)){//Aqui é para Letra (Se primeira letra for Char o restante pode ser Char ou Num somente, ex valido: Var520 | ex não valido 56var (Seria o estado q0 do AFD indo para o q1)
+                status = true;
 
-        boolean flag = true, status = true;
-
-        char primeiroCaracterAlpha = palavra.charAt(0);//pega o primeiro caracter da string
-
-        int tamPalavra = palavra.length();//tamanho da string
-
-        if (Character.isAlphabetic(primeiroCaracterAlpha)){//Aqui é para Letra (Se primeira letra for Char o restante pode ser Char ou Num somente, ex valido: Var520 | ex não valido 56var (Seria o estado q0 do AFD indo para o q1)
-            status = true;
-
-            for(i=0; i<tamPalavra; i++){
-                if (Character.isAlphabetic(palavra.charAt(i)) || Character.isDigit(palavra.charAt(i))){
-                    status = true;
-                } else {
-                    status = false;
-                }
-            }
-
-//            if(status == true){ insereTabela(letra);} Aqui insere na tabela caso, for letras e numeros token (ID)
-
-        }else if(Character.isDigit(palavra.charAt(0))){ //Aqui para numeros (Seria o estado q0 do AFD indo para o q6)
-            status = true;
-
-            for (i=0 ; i<tamPalavra ; i++){
-                if (Character.isDigit(palavra.charAt(i))){//Verifica se é somente numero
-                    status = true;
-
-                }else if(Character.isAlphabetic(palavra.charAt(i))){   //Verifica a presença de letra
-                    status = false;
-
-                }else if(palavra.contains(".")){//Verifica a presença do "."
-                    if(flag){
+                for(i=0; i<tamPalavra; i++){
+                    if (Character.isAlphabetic(palavra.charAt(i)) || Character.isDigit(palavra.charAt(i))){
                         status = true;
-                        flag = false;
-
-                    }else {// Caso passe o numero de "."
+                    } else {
                         status = false;
                     }
                 }
+
+                if(status == true){
+                    linhaTabela = new LinhaTabela(contId, palavra, "ID", "-", "-", "-", "-", i);
+                    insereTabela(linhaTabela, nLinha);
+                }
+            }else if(Character.isDigit(palavra.charAt(0))){ //Aqui para numeros (Seria o estado q0 do AFD indo para o q6)
+                status = true;
+
+                for (i=0 ; i<tamPalavra ; i++){
+                    if (Character.isDigit(palavra.charAt(i))){//Verifica se é somente numero
+                        status = true;
+
+                    }else if(Character.isAlphabetic(palavra.charAt(i))){   //Verifica a presença de letra
+                        status = false;
+
+                    }else if(palavra.contains(".")){//Verifica a presença do "."
+                        if(flag){
+                            status = true;
+                            flag = false;
+
+                        }else {// Caso passe o numero de "."
+                            status = false;
+                        }
+                    }
+                }
+                if(status == true){
+                    linhaTabela = new LinhaTabela(contId, palavra, "NUM", "-", "-", "-", "-", i);
+                    insereTabela(linhaTabela, nLinha);
+                }
+
+            } else if (!Character.isAlphabetic(palavra.charAt(i)) && !Character.isDigit(palavra.charAt(i))){  // Aqui para os casos onde  não é uma palavra inciado com (A-Z) e (0-9)
+                linhaTabela = new LinhaTabela(contId, palavra, "ERRO", "-", "-", "-", "-", i);
+                insereTabela(linhaTabela, nLinha);
             }
-//            if(status == true){ insereTabela(numero);} Aqui insere na tabela caso, for numero token (NUM)
-
-        }else if (!Character.isAlphabetic(palavra.charAt(i)) && !Character.isDigit(palavra.charAt(i))){  // Aqui para os casos onde  não é uma palavra inciado com (A-Z) e (0-9)
-            status = false;
         }
-
-        return status; //Retorna o status do AFD, caso true palavra aceita\ caso false palavra rejeitada
-
     }
 
-    public static void insereTabela(LinhaTabela palavra, int numeroLinha){
-
+    public static void insereTabela(LinhaTabela linhaTabela, int numeroLinha){
+        // incremental do id da tabela
+        contId++;
+        listaLinhaTabela.add(linhaTabela);
     }
 
 //    public static void insereTabela(LinhaTabela palavra, int numeroLinha) { // passar linha por parametro
